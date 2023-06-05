16b99c636a905db8588e1ca5a36603108ba1fda1
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2023-06-05 08:51:45 -0300

    Classe Abstrata

---
+++ ClasseAbstrata/ClasseAbstrata/Forma.cs
@@ -0,0 +1,12 @@
+﻿namespace ClasseAbstrata;
+abstract class Forma
+{
+    public double Area { get; set; }
+    public double Perimetro { get; set; }
+    public abstract void CalcularPerimetro();
+    public abstract void CalcularArea();
+    public string Descricao()
+    {
+        return "Sua a classe abstrata Forma";
+    }
+}
---
+++ ClasseAbstrata/ClasseAbstrata/Program.cs
@@ -0,0 +1,13 @@
+﻿using ClasseAbstrata;
+
+Quadrado quadrado = new();
+Console.WriteLine(quadrado.Descricao());
+
+Console.WriteLine("Informe o valor do lado do quadrado em metros.");
+quadrado.Lado = Convert.ToDouble(Console.ReadLine());
+quadrado.CalcularArea();
+quadrado.CalcularPerimetro();
+
+Console.WriteLine("A área do quadrado é: " + quadrado.Area + " m2 ");
+Console.WriteLine("O perímetro do quadrado é: " + quadrado.Perimetro + " m ");
+
